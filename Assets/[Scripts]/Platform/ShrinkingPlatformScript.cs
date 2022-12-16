b1ce0c53e8c27ba01ade8e7ed7027ef17764f4bb
Philip Henderson <philip.henderson@hotmail.com>
2022-12-16 04:45:18 -0500

    Final Project Complete

--- Assets/[Scripts]/Platform/ShrinkingPlatformScript.cs
+++ Assets/[Scripts]/Platform/ShrinkingPlatformScript.cs
@@ -1,3 +1,21 @@
+//////////////////////////////////////////////
+///
+/// Source File Name : GAME2014-F2022-FinalTest-101137744
+/// Student Name: Philip Henderson
+/// StudentID: 101137744
+/// Last Date Modified: Decemeber 16th, 2022 Time: 4:30am
+/// Program Description: Platformer Game, Added Floating, 
+///                        Shrinking Platforms, Also Added Gems
+/// Revision History:
+///     V1.0 Added Shrinking Platforms
+///     V1.1 Added Gems
+///     V1.2 Added Sound Effects 
+/// 
+//////////////////////////////////////////////
+
+
+
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
